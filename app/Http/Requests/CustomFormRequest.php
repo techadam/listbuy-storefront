29709f3a0bad14fd1a6184642cfbe6259156fbd0
thecodearcher <brian.iyoha@gmail.com>
2019-09-16 01:30:54 +0100

    feat: create custom form request base class

---
+++ app/Http/Requests/CustomFormRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponseTrait;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CustomFormRequest extends FormRequest
+{
+    use ApiResponseTrait;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->badRequest("Validation Error",$validator->errors()));
+    }
+}
