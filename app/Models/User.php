c52bb4ab7e074860011ffab93913af76af78730a
thecodearcher <brian.iyoha@gmail.com>
2019-10-01 14:33:03 +0100

    feat: load user model `with` store

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected $with = ['store'];
+
     public function routeNotificationForTwilio()
     {
         return $this->phone;
