89a1fb56ec83776a671c43e8af3b17f5de07a184
thecodearcher <brian.iyoha@gmail.com>
2019-10-03 11:42:56 +0100

    feat: remove `/store/{store-slug}/products` from protected routes

--- routes/api.php
+++ routes/api.php
@@ -13,55 +13,64 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::namespace ('Api')->prefix('auth')->group(function () {
-    Route::post('/register', 'AuthController@register');
-    Route::post('/login', 'AuthController@login');
-    Route::post('/verify/{user}', 'AuthController@verify');
+Route::namespace ('Api')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', 'AuthController@register');
+        Route::post('/login', 'AuthController@login');
+        Route::post('/verify/{user}', 'AuthController@verify');
 
-    Route::get('/password/verify/{otp}', 'ForgotPasswordController@verifyResetCode');
-    Route::post('/password/request', 'ForgotPasswordController@sendResetCodeEmail');
-    Route::post('/password/reset', 'ForgotPasswordController@resetPassword');
-});
-
-Route::namespace ('Api')->prefix('import')->group(function () {
-    Route::post('/states', 'ImportController@importStates');
-    Route::post('/delivery/zone/tariff', 'ImportController@importZoneTariff');
-    Route::post('/delivery/zones', 'ImportController@importDeliveryZones');
-    Route::post('/delivery/weight', 'ImportController@importDeliveryWeight');
-    Route::post('/delivery/tariffs', 'ImportController@importDeliveryTariffs');
-});
-
-Route::namespace ('Api')->middleware(['auth.jwt'])->group(function () {
-
-    Route::prefix('store')->group(function () {
-        Route::get('/me', 'StoreController@getUserStore');
-        Route::post('/', 'StoreController@store');
-        Route::put('/{store}', 'StoreController@update');
-        Route::get('/{store}/products', 'StoreController@getStoreProducts');
-        Route::post('{store}/bank', 'StoreBankDetailsController@saveUserStoreBankDetails');
-        Route::put('{store}/bank', 'StoreBankDetailsController@updateUserStoreBankDetails');
-      });
+        Route::get('/password/verify/{otp}', 'ForgotPasswordController@verifyResetCode');
+        Route::post('/password/request', 'ForgotPasswordController@sendResetCodeEmail');
+        Route::post('/password/reset', 'ForgotPasswordController@resetPassword');
+    });
 
     Route::prefix('product')->group(function () {
-        Route::post('/', 'ProductsController@addProduct');
         Route::get('/{slug}', 'ProductsController@getProduct');
-        Route::put('/{product}', 'ProductsController@updateProduct');
     });
 
-    Route::prefix('products')->group(function () {
-        Route::get('/', 'ProductsController@getProducts');
-        Route::get('/active', 'ProductsController@getActiveStoresProducts');
+    Route::prefix('store')->group(function () {
+        Route::get('/{store}/products', 'StoreController@getStoreProducts');
     });
 
-    Route::prefix('order')->group(function () {
-        Route::get('/me', 'OrdersController@getUserOrders');
-        Route::post('/process', 'OrdersController@processOrder');
-    });
+    /* Authenticated routes */
+    Route::middleware(['auth.jwt'])->group(function () {
+        Route::prefix('store')->group(function () {
+            Route::get('/me', 'StoreController@getUserStore');
+            Route::post('/', 'StoreController@store');
+            Route::put('/{store}', 'StoreController@update');
+            Route::post('{store}/bank', 'StoreBankDetailsController@saveUserStoreBankDetails');
+            Route::put('{store}/bank', 'StoreBankDetailsController@updateUserStoreBankDetails');
+        });
 
-    Route::prefix('delivery')->group(function () {
-        Route::get('/price', 'DeliveryController@getDeliveryPrice');
-        Route::get('/states', 'DeliveryController@getDeliveryStates');
-    });
+        Route::prefix('product')->group(function () {
+            Route::post('/', 'ProductsController@addProduct');
+            Route::get('/{slug}', 'ProductsController@getProduct');
+            Route::put('/{product}', 'ProductsController@updateProduct');
+        });
 
+        Route::prefix('products')->group(function () {
+            Route::get('/', 'ProductsController@getProducts');
+            Route::get('/active', 'ProductsController@getActiveStoresProducts');
+        });
+
+        Route::prefix('order')->group(function () {
+            Route::get('/me', 'OrdersController@getUserOrders');
+            Route::post('/process', 'OrdersController@processOrder');
+        });
+
+        Route::prefix('delivery')->group(function () {
+            Route::get('/price', 'DeliveryController@getDeliveryPrice');
+            Route::get('/states', 'DeliveryController@getDeliveryStates');
+        });
+
+        Route::prefix('import')->group(function () {
+            Route::post('/states', 'ImportController@importStates');
+            Route::post('/delivery/zone/tariff', 'ImportController@importZoneTariff');
+            Route::post('/delivery/zones', 'ImportController@importDeliveryZones');
+            Route::post('/delivery/weight', 'ImportController@importDeliveryWeight');
+            Route::post('/delivery/tariffs', 'ImportController@importDeliveryTariffs');
+        });
+
+    });
 
 });
