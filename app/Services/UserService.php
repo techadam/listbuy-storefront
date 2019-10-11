26f699d47dfca65e73f27ae9179a1fb6cc1a87a8
thecodearcher <brian.iyoha@gmail.com>
2019-10-11 13:58:31 +0100

    feat: add method to get all users

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -15,4 +15,13 @@ class UserService
     {
         return User::with($with)->where('username', $username)->firstOrFail();
     }
+
+    public function getAllUsers($limit = 50, $role = null)
+    {
+        if ($role) {
+            return User::where('role', $role)->paginate($limit);
+        }
+
+        return User::paginate($limit);
+    }
 }
