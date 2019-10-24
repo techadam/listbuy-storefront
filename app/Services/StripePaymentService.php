6fc5d143197936ea8a880e43d7941b77a75725fa
thecodearcher <brian.iyoha@gmail.com>
2019-10-24 15:39:46 +0100

    fix: convert amount back to inital value before storing

--- app/Services/StripePaymentService.php
+++ app/Services/StripePaymentService.php
@@ -16,7 +16,7 @@ class StripePaymentService implements PaymentServiceInterface
         if (isset($response->status) && $response->status == "succeeded") {
             $data['status'] = "paid";
             $data['reference_id'] = $response->id;
-            $data['amount'] = $response->amount;
+            $data['amount'] = $response->amount/100;
             $data['currency'] = $response->currency;
             $data['payment_processor'] = Constants::STRIPE_PAYMENT;
             $data['payment_method'] = $response->payment_method;
@@ -33,8 +33,7 @@ class StripePaymentService implements PaymentServiceInterface
         try {
             $key = env('APP_ENV') == "local" ? env('STRIPE_TEST_KEY') : env('STRIPE_LIVE_KEY');
             Stripe::setApiKey($key);
-            /* TODO: check for amount calculation */
-            $charge = Charge::create(['amount' => ($order_data['total_price']), 'currency' => $order_data['currency'] ? $order_data['currency'] : 'usd', 'source' => $order_data['stripe_source']]);
+            $charge = Charge::create(['amount' => ($order_data['total_price'] * 100), 'currency' => $order_data['currency'] ? $order_data['currency'] : 'usd', 'source' => $order_data['stripe_source']]);
             return $charge;
         } catch (\Exception $error) {
             return ['error' => $error->getMessage()];
